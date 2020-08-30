fc34f46dfd4c323e7984fc878e017c969ba06fcd
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-30 02:55:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,6 @@ def speak_to_grandma(input)
   elsif input == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else 
-    return "HUH?! SPEAK UP, SOMMY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
